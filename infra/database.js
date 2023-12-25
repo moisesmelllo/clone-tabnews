d2fe0cf7aaf8f679bc598d24f400f5e62049764e
Moises Melo Viana Ferreira <125166797+moisesmelllo@users.noreply.github.com>
2023-12-25 22:30:18 +0000

    removing time limit database connections

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,6 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    idleTimeoutMillis: 100,
   });
   await client.connect();
 
