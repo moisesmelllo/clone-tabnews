a611505c293965141554e7509117f69752bb1d33
Moises Melo Viana Ferreira <125166797+moisesmelllo@users.noreply.github.com>
2024-02-02 01:22:43 +0000

    makes  `database.js` `client.connet()` more robust to errors

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,8 +2,6 @@ import database from 'infra/database.js'
 
 async function status(request, response) {
   const updateAt = new Date().toISOString();
-  
-  
 
   const databaseVersionResult = await database.query('SHOW server_version;')
   const databaseVersionValue = databaseVersionResult.rows[0].server_version
