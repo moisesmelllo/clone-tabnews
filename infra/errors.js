8f7ac14e6ab627bfbd1e0f67cf8aff41968cf7da
Moises Melo Viana Ferreira <125166797+moisesmelllo@users.noreply.github.com>
2025-01-08 00:33:57 +0000

    fix: fix linting

--- infra/errors.js
+++ infra/errors.js
@@ -3,17 +3,17 @@ export class InternalServerError extends Error {
     super("Um error interno não esperado aconteceu", {
       cause,
     });
-    this.name = "InternalServerError"
-    this.action = "Entre em contato com o suporte"
-    this.statusCode = 500
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte";
+    this.statusCode = 500;
   }
- 
+
   toJSON() {
     return {
       name: this.name,
       message: this.message,
       action: this.action,
       status_code: this.statusCode,
-    }
+    };
   }
 }
