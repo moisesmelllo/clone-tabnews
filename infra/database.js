e670f4bd4bd9c5f8081bf78bee02e8e786bba018
Moises Melo Viana Ferreira <125166797+moisesmelllo@users.noreply.github.com>
2025-01-26 19:14:39 +0000

    feat: padronize the controllers

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -7,9 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Erro dentro do catch do controller");
-    console.error(error);
-    throw error;
+    const ServiceErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query",
+      cause: error,
+    });
+    throw ServiceErrorObject;
   } finally {
     await client?.end();
   }
---
+++ tests/integration/api/v1/status/post.test.js
@@ -0,0 +1,25 @@
+import orchestrator from "../../../../orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("POST /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status", {
+        method: "POST",
+      });
+      expect(response.status).toBe(405);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Metodo não permitido para este endpoint",
+        action: "Verifique se o metodo HTTP é valido para este endpoint",
+        status_code: 405,
+      });
+    });
+  });
+});
